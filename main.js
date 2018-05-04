c87a7aba06215ea53e0399cb34f0718c4d1428e5
Kevin Ho <ho.kevins@gmail.com>
2018-05-03 21:49:31 -0400

    change port to process.env.PORT || 1337

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const models = require('./server/db/models');
 const app = require('./server');
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 
 // If you update your db schemas, make sure you drop the tables first and then recreate them with force: true!
 models.db.sync({})
